ae3821990d63ea7dbb97530d62d191f4af33794c
Ibrahim Sheikh <oprah@google.com>
2017-01-10 15:20:24 -0800

    Added files

---
+++ aggregate.js
@@ -0,0 +1,6 @@
+javascript(function(){
+	var%20script=document.createElement('script');
+	script.type='text/javascript';
+	script.src='http://mywebsite.com/script.js';
+	document.getElementsByTagName('head')[0].appendChild(script);
+})();
