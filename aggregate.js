13a8331f7abab6848c81b90180b3c31bae0e3323
Ibrahim Sheikh <oprah@google.com>
2017-01-10 15:25:18 -0800

    Test

--- aggregate.js
+++ aggregate.js
@@ -1,6 +1,1 @@
-javascript(function(){
-	var%20script=document.createElement('script');
-	script.type='text/javascript';
-	script.src='http://mywebsite.com/script.js';
-	document.getElementsByTagName('head')[0].appendChild(script);
-})();
+alert('Hi');
